041a03f46a5739d06b6248282015d82a388b7ea2
marlon-buckley <marlon.buckley@protonmail.com>
2023-07-20 08:10:39 +0100

    add comment on strict mode:

--- src/index.js
+++ src/index.js
@@ -3,4 +3,6 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
+// removed strict mode here becauase it was
+// rendering two instances of the Prosemirror editor
 root.render(<App />);
